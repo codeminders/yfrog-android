bee4bf8ffcd3f7a64473895294fed43d0a33e381
igord <igord@localhost>
2009-11-17 13:40:34 +0000

    Ticket #99, #101, #102, #105

--- YFrogAndroid/src/com/codeminders/yfrog/android/util/AlertUtils.java
+++ YFrogAndroid/src/com/codeminders/yfrog/android/util/AlertUtils.java
@@ -30,15 +30,15 @@ public final class AlertUtils {
 	public static final int TWITTER_CONNECTION_ERROR = -1;
 	
 	// http://apiwiki.twitter.com/HTTP-Response-Codes-and-Errors
-    private static final int NOT_MODIFIED = 304;// Not Modified: There was no new data to return.
-    private static final int BAD_REQUEST = 400;// Bad Request: The request was invalid.  An accompanying error message will explain why. This is the status code will be returned during rate limiting.
-    private static final int NOT_AUTHORIZED = 401;// Not Authorized: Authentication credentials were missing or incorrect.
-    private static final int FORBIDDEN = 403;// Forbidden: The request is understood, but it has been refused.  An accompanying error message will explain why.
-    private static final int NOT_FOUND = 404;// Not Found: The URI requested is invalid or the resource requested, such as a user, does not exists.
-    private static final int NOT_ACCEPTABLE = 406;// Not Acceptable: Returned by the Search API when an invalid format is specified in the request.
-    private static final int INTERNAL_SERVER_ERROR = 500;// Internal Server Error: Something is broken.  Please post to the group so the Twitter team can investigate.
-    private static final int BAD_GATEWAY = 502;// Bad Gateway: Twitter is down or being upgraded.
-    private static final int SERVICE_UNAVAILABLE = 503;// Service Unavailable: The Twitter servers are up, but overloaded with requests. Try again later. The search and trend methods use this to indicate when you are being rate limited.
+    public static final int NOT_MODIFIED = 304;// Not Modified: There was no new data to return.
+    public static final int BAD_REQUEST = 400;// Bad Request: The request was invalid.  An accompanying error message will explain why. This is the status code will be returned during rate limiting.
+    public static final int NOT_AUTHORIZED = 401;// Not Authorized: Authentication credentials were missing or incorrect.
+    public static final int FORBIDDEN = 403;// Forbidden: The request is understood, but it has been refused.  An accompanying error message will explain why.
+    public static final int NOT_FOUND = 404;// Not Found: The URI requested is invalid or the resource requested, such as a user, does not exists.
+    public static final int NOT_ACCEPTABLE = 406;// Not Acceptable: Returned by the Search API when an invalid format is specified in the request.
+    public static final int INTERNAL_SERVER_ERROR = 500;// Internal Server Error: Something is broken.  Please post to the group so the Twitter team can investigate.
+    public static final int BAD_GATEWAY = 502;// Bad Gateway: Twitter is down or being upgraded.
+    public static final int SERVICE_UNAVAILABLE = 503;// Service Unavailable: The Twitter servers are up, but overloaded with requests. Try again later. The search and trend methods use this to indicate when you are being rate limited.
 
     // http://code.google.com/p/imageshackapi/wiki/YFROGuploadAndPost
     public static final int YFROG_ERROR_1001 = 1001;// Invalid twitter username or password

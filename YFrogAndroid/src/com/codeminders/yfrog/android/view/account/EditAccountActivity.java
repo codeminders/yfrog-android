78a380509c5f6c87f44ea37302b4daa9ba1d07a1
dzirtbry <dzirtbry@codeminders.com>
2011-04-05 19:20:13 +0300

    Removed basic authentication from account managment page.

--- YFrogAndroid/src/com/codeminders/yfrog/android/view/account/EditAccountActivity.java
+++ YFrogAndroid/src/com/codeminders/yfrog/android/view/account/EditAccountActivity.java
@@ -7,7 +7,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.AlertDialog.Builder;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
@@ -16,7 +15,6 @@ import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.*;
 
 import com.codeminders.yfrog.android.R;
@@ -27,7 +25,6 @@ import com.codeminders.yfrog.android.controller.service.AccountService;
 import com.codeminders.yfrog.android.controller.service.ServiceFactory;
 import com.codeminders.yfrog.android.model.Account;
 import com.codeminders.yfrog.android.util.AlertUtils;
-import com.codeminders.yfrog.android.util.StringUtils;
 import com.codeminders.yfrog.android.util.async.AsyncYFrogUpdater;
 
 /**
@@ -60,28 +57,21 @@ public class EditAccountActivity extends Activity implements OnClickListener {
 		if (isEdit) {
 			editable = (Account) extras.getSerializable(KEY_EDITABLE);
 			TextView accountName = (TextView) findViewById(R.id.ae_name);
-			
-			EditText nickname = (EditText) findViewById(R.id.ae_nickname);
-			EditText password = (EditText) findViewById(R.id.ae_password);
+
 			TextView oauthStatus = (TextView) findViewById(R.id.ae_oath_status);
-			
-			nickname.setText(editable.getUsername());
+
 			accountName.setText(editable.getUsername());
-			password.setText(editable.getPassword());
-			setAuthMethod(editable.getAuthMethod());
 			oauthStatus.setText(editable.isOAuthVerified() ? 
 					R.string.ae_oauth_status_authorized : R.string.ae_oauth_status_need_authorize);
 		} else {
 			TableLayout layout = (TableLayout) findViewById(R.id.ae_account_name);
-			layout.setVisibility(View.GONE);
+			layout.setVisibility(View.GONE);	
 			editable = new Account();
+			editable.setAuthMethod(Account.METHOD_OAUTH);
 		}
 		
 		setTitle(isEdit ? R.string.edit_account_activity_title : R.string.add_account_activity_title);
-
-		setAuthMethodListener((RadioButton) findViewById(R.id.ae_common_method_rbutton));
-		setAuthMethodListener((RadioButton) findViewById(R.id.ae_oauth_method_rbutton));
-		
+	
 		Button button = (Button) findViewById(R.id.ae_save_button);
 		button.setText(!editable.isOAuthVerified() && editable.getAuthMethod() == Account.METHOD_OAUTH ? R.string.next : R.string.save);
 		Drawable d = !editable.isOAuthVerified() && editable.getAuthMethod() == Account.METHOD_OAUTH ?
@@ -93,93 +83,9 @@ public class EditAccountActivity extends Activity implements OnClickListener {
 		textView.setMovementMethod(LinkMovementMethod.getInstance());
 	}
 
-	private void setAuthMethodListener(RadioButton radioButton) {
-		if (radioButton.isChecked()) {
-			setShowLayout(radioButton.getId());
-		}
-		radioButton.setOnClickListener(authMethodListener);		
-	}
-	
-	private void setShowLayout(int radioId) {
-		Button button = (Button) findViewById(R.id.ae_save_button);
-		View common = (View) findViewById(R.id.ae_common_layout);
-		View oauth = (View) findViewById(R.id.ae_oauth_layout);
-		switch (radioId) {
-		case R.id.ae_common_method_rbutton:
-			common.setVisibility(View.VISIBLE);
-			oauth.setVisibility(View.GONE);
-			button.setText(R.string.save);
-			Drawable d = getResources().getDrawable(R.drawable.save);
-			button.setCompoundDrawablesWithIntrinsicBounds(null, null, d, null);
-
-			break;
-
-		case R.id.ae_oauth_method_rbutton:
-			common.setVisibility(View.GONE);
-			oauth.setVisibility(View.VISIBLE);
-			if (editable.getOauthStatus() != Account.OAUTH_STATUS_VERIFIED) { 
-				button.setText(R.string.next);
-				d = getResources().getDrawable(R.drawable.next);
-				button.setCompoundDrawablesWithIntrinsicBounds(null, null, d, null);
-			}
-			
-			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-			imm.hideSoftInputFromWindow(button.getWindowToken(), InputMethodManager.RESULT_UNCHANGED_SHOWN);
-			break;
-		}		
-	}
-
-	private void setAuthMethod(int method) {
-		RadioGroup radioGroup = (RadioGroup) findViewById(R.id.ae_auth_methods);
-		switch (method) {
-		case Account.METHOD_COMMON:
-			radioGroup.check(R.id.ae_common_method_rbutton);
-			break;
-		case Account.METHOD_OAUTH:
-			radioGroup.check(R.id.ae_oauth_method_rbutton);
-			break;
-		}
-	}
-	
-	private int getAuthMethod() {
-		RadioGroup radioGroup = (RadioGroup) findViewById(R.id.ae_auth_methods);
-		switch(radioGroup.getCheckedRadioButtonId()) {
-		case R.id.ae_common_method_rbutton:
-			return Account.METHOD_COMMON;
-
-		case R.id.ae_oauth_method_rbutton:
-			return Account.METHOD_OAUTH;
-		}
-		return Account.METHOD_COMMON;
-	}
-	
 	@Override
 	public void onClick(View v) {
-		int authMethod = getAuthMethod();
-		String nickname = ((EditText) findViewById(R.id.ae_nickname)).getText().toString();
-		String password = ((EditText) findViewById(R.id.ae_password)).getText().toString();
-		
-		if (authMethod == Account.METHOD_COMMON) {
-			if (StringUtils.isEmpty(nickname)) {
-				showDialog(ALERT_NICKNAME);
-				return;
-			}
-			
-			if (StringUtils.isEmpty(password)) {
-				showDialog(ALERT_PASSWORD);
-				return;			
-			}
-			
-			editable.setUsername(nickname);
-			editable.setPassword(password);
-
-			if (!accountService.isAccountUnique(editable)) {
-				showDialog(ALERT_ACCOUNT_NOT_UNIQUE);
-				return;
-			}
-		} 
-
-		editable.setAuthMethod(authMethod);
+		editable.setAuthMethod(Account.METHOD_OAUTH);
 
 		if (editable.isNeedOAuthAuthorization()) {
 			new AsyncYFrogUpdater(this) {
@@ -275,10 +181,4 @@ public class EditAccountActivity extends Activity implements OnClickListener {
 
 	}
 	
-	OnClickListener authMethodListener = new OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			setShowLayout(v.getId());
-		}
-	};
 }

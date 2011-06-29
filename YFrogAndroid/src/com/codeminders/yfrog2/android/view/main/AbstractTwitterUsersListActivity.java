d5585a4118c60b858be5b4456b11ab1ad66c5d8b
Igor Giziy <linsalion@codeminders.com>
2011-06-29 13:09:42 +0300

    Fixed issue #17

--- YFrogAndroid/src/com/codeminders/yfrog2/android/view/main/AbstractTwitterUsersListActivity.java
+++ YFrogAndroid/src/com/codeminders/yfrog2/android/view/main/AbstractTwitterUsersListActivity.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.codeminders.yfrog2.android.view.main;
 
@@ -23,6 +23,7 @@ import com.codeminders.yfrog2.android.model.TwitterUser;
 import com.codeminders.yfrog2.android.util.AlertUtils;
 import com.codeminders.yfrog2.android.util.StringUtils;
 import com.codeminders.yfrog2.android.util.async.AsyncYFrogUpdater;
+import com.codeminders.yfrog2.android.view.account.ListAccountsActivity;
 import com.codeminders.yfrog2.android.view.adapter.TwitterUserAdapter;
 import com.codeminders.yfrog2.android.view.message.WriteStatusActivity;
 import com.codeminders.yfrog2.android.view.user.UserDetailsActivity;
@@ -38,14 +39,14 @@ public abstract class AbstractTwitterUsersListActivity extends ListActivity {
 
 	private static final int ATTEMPTS_TO_RELOAD = 5;
 	private int attempts = 0;
-	
+
 	protected int selected = -1;
 
 	protected TwitterService twitterService;
 	protected ArrayList<TwitterUser> users = new ArrayList<TwitterUser>(0);
 
 	/**
-	 * 
+	 *
 	 */
 	public AbstractTwitterUsersListActivity() {
 		super();
@@ -54,38 +55,38 @@ public abstract class AbstractTwitterUsersListActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		twitterService = ServiceFactory.getTwitterService();
-		
+
 		boolean restored = restoreState(savedInstanceState);
-		
+
 		createList(!restored);
 	}
-	
+
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		saveState(outState);
 		super.onSaveInstanceState(outState);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	protected boolean restoreState(Bundle savedState) {
 		if (savedState == null) {
 			return false;
 		}
-		
+
 		Serializable values = savedState.getSerializable(SAVED_USERS);
 		if (values == null) {
 			return false;
 		}
-		users = (ArrayList<TwitterUser>) values;		
-		
+		users = (ArrayList<TwitterUser>) values;
+
 		int value = savedState.getInt(SAVED_ATTEMPTS);
 		if (value < 0) {
 			return false;
 		}
 		attempts = value;
-		
+
 		value = savedState.getInt(SAVED_SELECTED);
 		if (value > -1 && value < users.size()) {
 			selected = value;
@@ -93,15 +94,15 @@ public abstract class AbstractTwitterUsersListActivity extends ListActivity {
 
 		return true;
 	}
-	
+
 	private void saveState(Bundle savedState) {
 		if (savedState == null) {
 			return;
 		}
-		
+
 		savedState.putSerializable(SAVED_USERS, users);
 		savedState.putInt(SAVED_ATTEMPTS, attempts);
-		
+
 		try {
 			savedState.putInt(SAVED_SELECTED, getSelectedItemPosition());
 		} catch (Exception e) {
@@ -119,19 +120,19 @@ public abstract class AbstractTwitterUsersListActivity extends ListActivity {
 		if (twitterUpdate) {
 			attempts = 1;
 		}
-		
+
 		boolean needReload = twitterUpdate || isNeedReload();
-		
+
 		if (needReload) {
 			new AsyncYFrogUpdater(this) {
 				protected void doUpdate() throws YFrogTwitterException {
 					users = getUsers();
 				}
-				
+
 				protected void doAfterUpdate() {
 					show();
 				}
-				
+
 				protected void doAfterError() {
 					show();
 				}
@@ -139,23 +140,23 @@ public abstract class AbstractTwitterUsersListActivity extends ListActivity {
 		} else {
 			show();
 		}
-		
-		
+
+
 	}
 
 	private void show() {
 		setContentView(R.layout.twitter_users_list);
 		setListAdapter(new TwitterUserAdapter<TwitterUser>(this, users));
-		
+
 		if (selected > -1) {
 			setSelection(selected);
 		}
-		
+
 		getListView().setTextFilterEnabled(true);
 		registerForContextMenu(getListView());
 		setTitle(createTitle());
 	}
-	
+
 	private boolean isNeedReload() {
 		return (++attempts % ATTEMPTS_TO_RELOAD == 0);
 	}
@@ -164,10 +165,10 @@ public abstract class AbstractTwitterUsersListActivity extends ListActivity {
 	protected void onListItemClick(ListView l, View v, int position,
 			long id) {
 				Intent intent = new Intent(this, UserDetailsActivity.class);
-				
+
 				intent.putExtra(UserDetailsActivity.KEY_USERS, users);
 				intent.putExtra(UserDetailsActivity.KEY_USER_POS, position);
-				
+
 				startActivityForResult(intent, 0);
 	}
 
@@ -175,38 +176,38 @@ public abstract class AbstractTwitterUsersListActivity extends ListActivity {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode == RESULT_OK) {
 			Bundle extras = data.getExtras();
-			
+
 			if (extras != null) {
 				Serializable serializable = extras.getSerializable(UserDetailsActivity.KEY_USERS);
 				if (serializable != null) {
 					users = (ArrayList<TwitterUser>) serializable;
 				}
-				
+
 				int pos = extras.getInt(UserDetailsActivity.KEY_USER_POS);
-				
+
 				if (pos > -1 && pos < users.size()) {
 					selected = pos;
 				}
-				
+
 			}
 		}
 	}
-	
-	protected abstract ArrayList<TwitterUser> getUsers() throws YFrogTwitterException; 
-	
+
+	protected abstract ArrayList<TwitterUser> getUsers() throws YFrogTwitterException;
+
 	@Override
 	protected Dialog onCreateDialog(int id) {
 		return AlertUtils.createErrorAlert(this, id);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.common_refresh_list, menu);
 		getMenuInflater().inflate(R.menu.common_add_tweet, menu);
 		return super.onCreateOptionsMenu(menu);
-		
+
 	}
-	
+
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.reload_list:
@@ -220,7 +221,14 @@ public abstract class AbstractTwitterUsersListActivity extends ListActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	protected String createTitle() {
-		return StringUtils.formatTitle(twitterService.getLoggedUser().getUsername());
-	}
+    protected String createTitle() {
+        try {
+            return StringUtils.formatTitle(twitterService.getLoggedUser().getUsername());
+        } catch (NullPointerException e) {
+            startActivity(new Intent(this, ListAccountsActivity.class));
+            finish();
+            return "";
+        }
+    }
+
 }
